dbe26e3fe22176b078c7ca72b062cc57b1ffc10a
Vivek Meena <vivek.meena.ops@gmail.com>
2026-01-18 14:40:56 +0530

    backend update and code update

--- backend.tf
+++ backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "terraform-state-manage-s3-bucket-for-all-dem0"
     key            = "infra-setup/terraform.tfstate"
     region         = "us-east-1"
-    encrypt        = true
+
     use_lockfile   = true
   }
 }
